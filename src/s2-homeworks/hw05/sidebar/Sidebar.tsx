4c92d947fbddffcd2cebd142c85936fb7e975d5e
Mikhail <koz.mikhail.s@yandex.ru>
2022-12-29 18:59:34 +0300

    DZ-5

--- src/s2-homeworks/hw05/sidebar/Sidebar.tsx
+++ src/s2-homeworks/hw05/sidebar/Sidebar.tsx
@@ -3,6 +3,7 @@ import {NavLink} from 'react-router-dom'
 import s from './Sidebar.module.css'
 import {PATH} from '../Pages'
 import closeIcon from './closeOutline.svg'
+import { style } from '@mui/system'
 
 type PropsType = {
     open: boolean
@@ -31,7 +32,7 @@ export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
                         id={'hw5-pre-junior-link'}
                         to={PATH.PRE_JUNIOR}
                         onClick={handleClose}
-                        // className={...} // делает студент
+                        className={({isActive}) => isActive ? s.active : " "} // делает студент
                     >
                         Pre-junior
                     </NavLink>
@@ -39,7 +40,7 @@ export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
                         id={'hw5-junior-link'}
                         to={PATH.JUNIOR}
                         onClick={handleClose}
-                        // className={...} // делает студент
+                        className={({isActive}) => isActive ? s.active : " "} // делает студент
                     >
                         Junior
                     </NavLink>
@@ -47,7 +48,7 @@ export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
                         id={'hw5-junior-plus-link'}
                         to={PATH.JUNIOR_PLUS}
                         onClick={handleClose}
-                        // className={...} // делает студент
+                        className={({isActive}) => isActive ? s.active : s.close} // делает студент
                     >
                         Junior Plus
                     </NavLink>
