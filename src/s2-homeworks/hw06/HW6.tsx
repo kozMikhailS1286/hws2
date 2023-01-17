849f037787c61e86b9ee9e93b306addf171a8172
Mikhail <koz.mikhail.s@yandex.ru>
2023-01-17 13:40:47 +0300

    DZ-6

--- src/s2-homeworks/hw06/HW6.tsx
+++ src/s2-homeworks/hw06/HW6.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import SuperEditableSpan from './common/c4-SuperEditableSpan/SuperEditableSpan'
 import { restoreState, saveState } from './localStorage/localStorage'
 import s2 from '../../s1-main/App.module.css'
@@ -19,7 +19,7 @@ const HW6 = () => {
     }
     const restore = () => {
         // делают студенты
-
+        localStorage.getItem('hw6-editable-span-value')
     }
 
     return (
