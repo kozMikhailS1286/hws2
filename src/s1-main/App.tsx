55c39a4deac013056e1c8752605b8ca550a69610
kozMikhailS1286 <koz.mikhail.s@yandex.ru>
2023-02-13 11:02:04 +0300

    DZ-7

--- src/s1-main/App.tsx
+++ src/s1-main/App.tsx
@@ -6,6 +6,7 @@ import s from './App.module.css'
 // import HW4 from '../s2-homeworks/hw04/HW4'
 import HW5 from '../s2-homeworks/hw05/HW5'
 import HW6 from '../s2-homeworks/hw06/HW6'
+import HW7 from "../s2-homeworks/hw07/HW7";
 
 function App() {
     return (
@@ -17,6 +18,7 @@ function App() {
             {/*при выполнении дз 5 и более - закомментировать здесь дз 1-4, так как они есть внутри дз 5*/}
             <HW5/>
             <HW6/>
+            <HW7/>
                 {/*раскомментировать по ходу выполнения*/}
         </div>
     )
