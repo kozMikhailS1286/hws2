0874892b0124095b91edee3b4864c5315a0b6ad3
kozMikhailS1286 <koz.mikhail.s@yandex.ru>
2024-03-05 22:41:50 +0300

    hw14

--- src/s1-main/App.tsx
+++ src/s1-main/App.tsx
@@ -13,6 +13,7 @@ import HW10 from "../s2-homeworks/hw10/HW10";
 import HW11 from "../s2-homeworks/hw11/HW11";
 import HW12 from "../s2-homeworks/hw12/HW12";
 import HW13 from "../s2-homeworks/hw13/HW13";
+import HW14 from "../s2-homeworks/hw14/HW14";
 
 function App() {
     return (
@@ -23,14 +24,15 @@ function App() {
             <HW4/> */}
             {/*при выполнении дз 5 и более - закомментировать здесь дз 1-4, так как они есть внутри дз 5*/}
             <HW5/>
-            <HW6/>
-            <HW7/>
-            <HW8/>
-            <HW9/>
-            <HW10/>
-            <HW11/>
-            <HW12/>
-            <HW13/>
+            {/*<HW6/>*/}
+            {/*<HW7/>*/}
+            {/*<HW8/>*/}
+            {/*<HW9/>*/}
+            {/*<HW10/>*/}
+            {/*<HW11/>*/}
+            {/*<HW12/>*/}
+            {/*<HW13/>*/}
+            {/*<HW14/>*/}
                 {/*раскомментировать по ходу выполнения*/}
         </div>
     )
